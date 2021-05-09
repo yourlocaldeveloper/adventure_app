bc3e0634235f83d03ba050fd71eda5e128887ddf
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-09 19:06:34 +0100

    :sparkles: :lipstick: add modal for game instructions/about project and style

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import castle from '../../images/castle.png';
 import dragon from '../../images/dragon.png';
 import penguin from '../../images/penguin.png';
 
 const Home = () => {
+    const [display, setDisplay] = useState(false)
 
     const history = useHistory();
 
@@ -12,8 +13,21 @@ const Home = () => {
         history.push("/game");
     }
 
+    const getHelp = () => {
+        setDisplay(!display)
+    }
+
     return (
         <>
+            <div className="help-container" style={display ? { display: "block" } : { display: "none" }}>
+                <div className="show-help">
+                    <p>This is a text-based adventure game created by Aaron & Natalie etc etc etc.. This is how to play and visit our repo plsssss</p>
+                </div>
+            </div>
+            <header id="help">
+                <button onClick={getHelp} aria-label="how to play and more info">??</button>
+            </header>
+
             <main id="homepage">
                 <div class="wrapper-homepage">
                     <div className="landing-images">
@@ -24,6 +38,7 @@ const Home = () => {
                     <div className="enter-button">
                         <button onClick={handleClick}>Enter The Castle</button>
                     </div>
+
                 </div>
             </main>
         </>
