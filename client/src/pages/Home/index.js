4a45ddbfba613aa2b27f6c881e45682f309f1001
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-09 16:08:33 +0100

    :sparkles: import and render images on landing pahe, remove placeholder header"

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import castle from '../../images/castle.png';
+import dragon from '../../images/dragon.png';
+import penguin from '../../images/penguin.png';
 
 const Home = () => {
  
     return (
         <>
-            <header>
-                <h1>Hello World!</h1>
-            </header>
+            <main id="homepage">
+                <div className="landing-images">
+                <img id="penguin-img" src={penguin}/>
+                <img id="castle-img" src={castle}/>
+                <img id="dragon-img" src={dragon}/>
+                </div>
+            </main>
         </>
     )
 }
