f5245a55fc4a61be98b00a45f36e6a580d18b1cd
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-09 17:49:19 +0100

    :zap: button onClick links to /game

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 import castle from '../../images/castle.png';
 import dragon from '../../images/dragon.png';
 import penguin from '../../images/penguin.png';
 
 const Home = () => {
- 
+
+    const history = useHistory();
+
+    const handleClick = () => {
+        history.push("/game");
+    }
+
     return (
         <>
             <main id="homepage">
                 <div class="wrapper-homepage">
                     <div className="landing-images">
-                    <img id="penguin-img" src={penguin}/>
-                    <img id="castle-img" src={castle}/>
-                    <img id="dragon-img" src={dragon}/>
+                        <img id="penguin-img" src={penguin} />
+                        <img id="castle-img" src={castle} />
+                        <img id="dragon-img" src={dragon} />
                     </div>
                     <div className="enter-button">
-                    <button>Enter The Castle</button>
+                        <button onClick={handleClick}>Enter The Castle</button>
                     </div>
                 </div>
             </main>
