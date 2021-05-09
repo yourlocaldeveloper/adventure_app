39537410849e9bad493188279aaa9456d3f3f7f8
HeyAero <aeroyts@icloud.com>
2021-05-09 21:13:28 +0100

    updating styling for progress bar

--- client/src/components/CLI/index.js
+++ client/src/components/CLI/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const CLI = () => {
- 
+
     return (
         <div id="cli">
           <div className="p-wrapper">
-            <p>Test1 hello I am a penguin weeeeeeeeeeeeee wooooooooo</p>
+            <p>Test1 hello I am a penguin weeeeeeeeeeeeee wooawdawdawdawdadw ooooooo adwadwd</p>
           </div>
         </div>
     )
