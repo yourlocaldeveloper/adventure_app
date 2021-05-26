ecca8442cf7a937cb5ccaaab5c8e44c4dc78b4f6
HeyAero <aeroyts@icloud.com>
2021-05-26 17:12:03 +0100

    update command line page to fetch story and include error/loading states

--- client/src/pages/CommandLine/index.js
+++ client/src/pages/CommandLine/index.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CLI, ProgressBar, InputBar } from '../../components'
+import axios from 'axios';
 
 const CommandLine = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [story, setStory] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchStories = async () => {
+      try {
+        const {data} = await axios.get("http://localhost:3000/story/")
+        setStory(data)
+        setLoading(false);
+      } catch(err) {
+        console.warn(err);
+        setError(err)
+        setLoading(false);
+      }
+    }
+
+    fetchStories();
+  }, [])
+
+  function fetchContent(){
+    console.log(story)
+    return (
+      <>
+        {error ?
+          <p>An error has occured.</p>
+        :
+          <>
+            <ProgressBar />
+            <CLI/>
+            <InputBar />
+          </>
+        }
+      </>
+    )
+  }
  
     return (
-        <section id="command-line" class="container">
-          <ProgressBar />
-          <CLI />
-          <InputBar />
+        <section id="command-line" className="container">
+          {
+            loading ?
+              <>
+                <h1>Loading page</h1>
+              </>
+            :
+              fetchContent()
+          }
         </section>
     )
 }
